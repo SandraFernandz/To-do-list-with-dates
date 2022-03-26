3e9f65eba05086d95b04289efcd0a3b25b5071ce
Sandra Fernandz <sandraenmunich@gmail.com>
2022-03-26 19:57:12 +0100

    created first structure

--- my-to-do-list-with-dates/src/App.tsx
+++ my-to-do-list-with-dates/src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { FC } from 'react';
 import './App.css';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <header className='header'>
+     <div className='inputContainer'>
+     <input type="text" placeholder='task...' />
+     <input type="number" placeholder='deadline (in days)' />
+     </div>
+     <button>Add Task</button>
+   </header>
+   <div className='todoList'></div>
     </div>
   );
 }
