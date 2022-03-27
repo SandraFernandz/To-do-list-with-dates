eb1c9f6dba9beae161a17df90084b6545a354c4f
Sandra Fernandz <sandraenmunich@gmail.com>
2022-03-27 10:50:18 +0200

    commit changes

--- my-to-do-list-with-dates/src/App.tsx
+++ my-to-do-list-with-dates/src/App.tsx
@@ -1,44 +1,61 @@
 import React, { ChangeEvent, useState } from 'react';
 import { FC } from 'react';
 import './App.css';
-import { ITask } from './interfaces'
+import { ITask } from './interfaces';
+import TodoTask from './Components/todoTask';
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [deadline, setDeadline] = useState<number>(0);
+  const [list, setList] = useState<ITask[]>([]);
 
-const[task, setTask] = useState<string>('');
-const[deadline, setDeadline] = useState<number>(0);
-const[list, setList] = useState<ITask[]>([]);
+  const handleTask = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
 
-const handleTask = (event: ChangeEvent<HTMLInputElement>):void =>{
-  if(event.target.name === 'task'){
-    setTask(event.target.value);
-  }else{
-    setDeadline(Number(event.target.value));
-  }
-};
+  const handleList = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setList([...list, newTask]);
+    setTask('');
+    setDeadline(0);
+  };
 
-const handleList = (): void => {
-  const newTask = { taskName: task, deadline: deadline};
-  
-  setList([...list, newTask]);
-  console.log(list);
-}
   return (
     <div className="App">
-   <header className='header'>
-     <div className='inputContainer'>
-     <input className='input' type="text" placeholder='task...' name='task' onChange={handleTask}/>
-     <input type="number" placeholder='deadline (in days)' name='days' onChange={handleTask}/>
-     </div>
-     <button className='button' onClick ={
-       handleList
-     }>add</button>
-     </header>
+      <header className="header">
+        <div className="inputContainer">
+          <input
+            className="input"
+            type="text"
+            placeholder="task..."
+            name="task"
+            onChange={handleTask}
+            value={task}
+          />
+          <input
+            type="number"
+            placeholder="deadline (in days)"
+            name="days"
+            onChange={handleTask}
+            value={deadline}
+          />
+        </div>
+        <button className="button" onClick={handleList}>
+          add
+        </button>
+      </header>
 
-     
-   <div className='todoList'></div>
+      <div className="todoList">
+        {list.map(() => {
+          return <TodoTask />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ my-to-do-list-with-dates/src/Components/TodoTask.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoTask (){
+  return(
+  <>
+  <div>task</div>
+  task
+  </>
+  )
+};
+
+export default TodoTask;
